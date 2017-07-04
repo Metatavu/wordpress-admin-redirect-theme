0d75c182de0b148002e6154e5d0f9b881f4fe512
Antti Leppä <antti.leppa@metatavu.fi>
2017-07-04 14:44:02 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Index template. Page redirects user to root path in the actual web pag
+ * 
+ * @package kunta-api-redirect-theme
+ * @since 1.0
+ * @version 1.0
+ */
+wp_redirect("/wp-admin/");
+exit;
+?>
